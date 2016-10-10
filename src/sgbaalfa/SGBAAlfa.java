2655182ce7970cef7180d9edbd99809dcd9e4c21
washingtonSR <washington.tads@gmail.com>
2016-10-10 10:29:58 -0300

    teste

--- src/sgbaalfa/SGBAAlfa.java
+++ src/sgbaalfa/SGBAAlfa.java
@@ -16,6 +16,7 @@ public class SGBAAlfa {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("suco de frutas");
     }
     
 }
