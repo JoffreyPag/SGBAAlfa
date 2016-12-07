6edb2b96163bfb7e1136d2a04223e302c4bede66
joffr <joffr@LAPTOP-0MMUFH2S>
2016-12-07 08:38:05 -0300

    att

--- src/sgbaalfa/SGBAAlfa.java
+++ src/sgbaalfa/SGBAAlfa.java
@@ -14,6 +14,7 @@ public class SGBAAlfa {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Senta e chora");
         new TelaBolsista().setVisible(true);
         //conexao con = new conexao();
         //con.conectar();
